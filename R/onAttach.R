efab2ebc5c92a460fe3a3c3d1556cb21b505e22f
LYING7797 <lying7797@outlook.com>
2025-01-21 17:32:13 +0800

    onAttach.R

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,9 +1,4 @@
 .onAttach <- function(libname, pkgname) {
-  # 检查数据是否已经存在，如果没有则下载
-  project_dir <- getwd()  # 获取当前工作目录
-  data_files <- list.files(project_dir, pattern = "\\.csv$")  # 查找是否有CSV文件
-  
-  if (length(data_files) == 0) {  # 如果没有CSV文件，下载数据
-    install_data()
-  }
+    # 自动下载数据文件
+  install_data()  # 调用 install_data 函数下载数据
 }
