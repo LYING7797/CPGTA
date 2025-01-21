cd3ed9c492287f8d27b628861f0ee2b3e4b2ccbb
LYING7797 <lying7797@outlook.com>
2025-01-21 17:18:43 +0800

    install_data.R

--- R/install_data.R
+++ R/install_data.R
@@ -1,5 +1,5 @@
 install_data <- function() {
-  # 加载googledrive包
+# 安装并加载 googledrive 包
   if (!requireNamespace("googledrive", quietly = TRUE)) {
     install.packages("googledrive")
   }
@@ -7,23 +7,16 @@ install_data <- function() {
 
   # Google Drive文件夹的ID
   folder_id <- "1Yr_msBFVwu26F4MwGBgbAu0255vb02Eq"
-
+  
   # 获取文件夹中的所有文件
-  files <- drive_find(n_max = 10, path = as_id(folder_id))
-
-  # 下载文件到本地目录（确保目录存在）
+  files <- drive_find(path = as_id(folder_id))  # 不限制最大文件数，获取文件夹中的所有文件
+  
+  # 确保安装目录存在
+  project_dir <- getwd()  # 获取当前工作目录，即包的根目录
+  
+  # 下载文件到包的根目录
   for (file in files$name) {
-    # 获取文件的路径
-    file_info <- drive_get(file)
-    file_path <- file_info$path
-
-    # 确保本地目录存在
-    local_dir <- dirname(file_path)
-    if (!dir.exists(local_dir)) {
-      dir.create(local_dir, recursive = TRUE)
-    }
-
-    # 下载文件到本地目录
+    file_path <- file.path(project_dir, file)  # 文件的本地存储路径，放在包的根目录下
     drive_download(file, path = file_path, overwrite = TRUE)
   }
 }
